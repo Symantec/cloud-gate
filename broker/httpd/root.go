a0fcbb89c2f05fa988e71410ad3bf34c8ad76e82
Richard Gooch <rg+github0@safe-mbox.com>
2018-04-16 08:03:50 -0700

    Add root and status pages to HTTP server.

---
+++ broker/httpd/root.go
@@ -0,0 +1,25 @@
+package httpd
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
+func (s *Server) rootHandler(w http.ResponseWriter, req *http.Request) {
+	writer := bufio.NewWriter(w)
+	defer writer.Flush()
+	fmt.Fprintln(writer, "<title>cloud-gate</title>")
+	fmt.Fprintln(writer, `<style>
+                          table, th, td {
+                          border-collapse: collapse;
+                          }
+                          </style>`)
+	fmt.Fprintln(writer, "<body>")
+	fmt.Fprintln(writer, "<center>")
+	fmt.Fprintln(writer, "<h1>cloud-gate UI. Under Construction.</h1>")
+	fmt.Fprintln(writer, "<hr>")
+	fmt.Fprintln(writer, "<a href=\"status\">Status page</a>")
+	fmt.Fprintln(writer, "</center>")
+	fmt.Fprintln(writer, "</body>")
+}
---
+++ broker/httpd/status.go
@@ -0,0 +1,38 @@
+package httpd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Symantec/Dominator/lib/html"
+)
+
+func (s *Server) statusHandler(w http.ResponseWriter, req *http.Request) {
+	writer := bufio.NewWriter(w)
+	defer writer.Flush()
+	fmt.Fprintln(writer, "<title>cloud-gate status page</title>")
+	fmt.Fprintln(writer, `<style>
+                          table, th, td {
+                          border-collapse: collapse;
+                          }
+                          </style>`)
+	fmt.Fprintln(writer, "<body>")
+	fmt.Fprintln(writer, "<center>")
+	fmt.Fprintln(writer, "<h1>cloud-gate status page</h1>")
+	fmt.Fprintln(writer, "</center>")
+	html.WriteHeaderWithRequest(writer, req)
+	fmt.Fprintln(writer, "<h3>")
+	s.writeDashboard(writer)
+	for _, htmlWriter := range s.htmlWriters {
+		htmlWriter.WriteHtml(writer)
+	}
+	fmt.Fprintln(writer, "</h3>")
+	fmt.Fprintln(writer, "<hr>")
+	html.WriteFooter(writer)
+	fmt.Fprintln(writer, "</body>")
+}
+
+func (s *Server) writeDashboard(writer io.Writer) {
+}
