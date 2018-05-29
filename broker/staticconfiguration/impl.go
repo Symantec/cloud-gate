54db51fa0bc653eb467f41c2df22e040ed380b09
Camilo Viecco <camilo_viecco1@symantec.com>
2018-05-29 15:49:08 +0000

    addressing comments

--- broker/staticconfiguration/impl.go
+++ broker/staticconfiguration/impl.go
@@ -2,15 +2,11 @@ package staticconfiguration
 
 import (
 	"errors"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"os"
-)
 
-const defaultServicePort = 443
-const defaultStatusPort = 6930
-const defaultAccountConfigurationUrl = "file:///etc/cloud-gate/accounts.yml"
-const defaultAccountConfigurationCheckInterval = "300s"
+	"github.com/Symantec/cloud-gate/lib/constants"
+	"gopkg.in/yaml.v2"
+)
 
 func LoadVerifyConfigFile(configFilename string) (*StaticConfiguration, error) {
 	var config StaticConfiguration
@@ -18,28 +14,29 @@ func LoadVerifyConfigFile(configFilename string) (*StaticConfiguration, error) {
 		err = errors.New("mising config file failure")
 		return nil, err
 	}
-	source, err := ioutil.ReadFile(configFilename)
+	source, err := os.Open(configFilename)
 	if err != nil {
-		err = errors.New("cannot read config file")
 		return nil, err
+
 	}
-	err = yaml.Unmarshal(source, &config)
+	err = yaml.NewDecoder(source).Decode(&config)
 	if err != nil {
-		err = errors.New("Cannot parse config file")
 		return nil, err
 	}
 	// setup defaults
 	if config.Base.StatusPort == 0 {
-		config.Base.StatusPort = defaultStatusPort
+		config.Base.StatusPort = constants.DefaultStatusPort
 	}
 	if config.Base.ServicePort == 0 {
-		config.Base.ServicePort = defaultServicePort
+		config.Base.ServicePort = constants.DefaultServicePort
 	}
 	if len(config.Base.AccountConfigurationUrl) == 0 {
-		config.Base.AccountConfigurationUrl = defaultAccountConfigurationUrl
+		config.Base.AccountConfigurationUrl =
+			constants.DefaultAccountConfigurationUrl
 	}
 	if len(config.Base.AccountConfigurationCheckInterval) == 0 {
-		config.Base.AccountConfigurationCheckInterval = defaultAccountConfigurationCheckInterval
+		config.Base.AccountConfigurationCheckInterval =
+			constants.DefaultAccountConfigurationCheckInterval
 	}
 
 	return &config, nil
---
+++ lib/constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	DefaultServicePort                       = 443
+	DefaultStatusPort                        = 6930
+	DefaultAccountConfigurationUrl           = "file:///etc/cloud-gate/accounts.yml"
+	DefaultAccountConfigurationCheckInterval = "300s"
+)
