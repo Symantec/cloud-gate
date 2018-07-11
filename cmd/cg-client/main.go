e0252548439196389f614834582413cde1c87b8c
Camilo Viecco <camilo_viecco1@symantec.com>
2018-07-11 15:56:33 -0700

    adding admin option and makefile

--- cmd/cg-client/main.go
+++ cmd/cg-client/main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"encoding/pem"
+	"errors"
 	"flag"
 	"io/ioutil"
 	//"fmt"
@@ -22,8 +23,9 @@ import (
 var (
 	certFilename         = flag.String("cert", filepath.Join(os.Getenv("HOME"), ".ssl", "keymaster.cert"), "A PEM eoncoded certificate file.")
 	keyFilename          = flag.String("key", filepath.Join(os.Getenv("HOME"), ".ssl", "keymaster.key"), "A PEM encoded private key file.")
-	baseURL              = flag.String("baseURL", "https://mon-sre-dev.us-east-1.aws.symcpe.net:32443", "location of the cloud-broker")
+	baseURL              = flag.String("baseURL", "https://cloud-gate-dev.symcpe.net", "location of the cloud-broker")
 	crededentialFilename = flag.String("credentialFile", filepath.Join(os.Getenv("HOME"), ".aws", "credentials"), "An Ini file with credentials")
+	askAdminRoles        = flag.Bool("askAdminRoles", false, "ask also for admin roles")
 )
 
 type cloudAccountInfo struct {
@@ -43,6 +45,9 @@ type AWSCredentialsJSON struct {
 	Region       string `json:"region,omitempty"`
 }
 
+const badReturnErrText = "bad return code"
+const sleepDuration = 1200 * time.Second
+
 func getAndUptateCreds(client *http.Client, baseUrl, accountName, roleName string, cfg *ini.File) error {
 	log.Printf("account=%s, role=%s", accountName, roleName)
 
@@ -57,6 +62,9 @@ func getAndUptateCreds(client *http.Client, baseUrl, accountName, roleName strin
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if resp.StatusCode >= 300 {
+		return errors.New(badReturnErrText)
 	}
 	//log.Println(string(data))
 
@@ -74,7 +82,7 @@ func getAndUptateCreds(client *http.Client, baseUrl, accountName, roleName strin
 	return nil
 }
 
-func getCerts(cert tls.Certificate, baseUrl string, credentialFilename string) error {
+func getCerts(cert tls.Certificate, baseUrl string, credentialFilename string, askAdminRoles bool) error {
 
 	//get credfile, TODO: make if ones does not exist
 	credFile, err := ini.Load(credentialFilename)
@@ -114,12 +122,19 @@ func getCerts(cert tls.Certificate, baseUrl string, credentialFilename string) e
 	for _, account := range accountList.CloudAccounts {
 		for _, roleName := range account.AvailableRoles {
 			adminRole, err := regexp.Match("(?i)admin", []byte(roleName))
-			if adminRole {
+			if adminRole && !askAdminRoles {
 				continue
+			}
+			if err != nil {
+				log.Fatalf("error on regexp=%s", err)
 			}
 			err = getAndUptateCreds(client, baseUrl, account.Name, roleName, credFile)
 			if err != nil {
-				log.Fatal(err)
+				if err.Error() == badReturnErrText {
+					log.Printf("skipping role")
+					continue
+				}
+				log.Fatalf("error on getAnd UpdateCreds=%s", err)
 			}
 		}
 	}
@@ -155,12 +170,11 @@ func main() {
 	}
 
 	for certNotAfter.After(time.Now()) {
-		sleepDuration := 1200 * time.Second
 		cert, err := tls.LoadX509KeyPair(*certFilename, *keyFilename)
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = getCerts(cert, *baseURL, *crededentialFilename)
+		err = getCerts(cert, *baseURL, *crededentialFilename, *askAdminRoles)
 		if err != nil {
 			log.Fatal(err)
 		}
