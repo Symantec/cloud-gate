11d20559bbfaf1fec84dba67d05ebfb2e2e3249e
Camilo Viecco <camilo_viecco1@symantec.com>
2018-07-25 17:34:24 +0000

    upping to version 0.6.1 to account for customization move

--- broker/httpd/static.go
+++ broker/httpd/static.go
@@ -13,11 +13,6 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%s", commonCSS)
 		return
 
-	case "/static/customization-old.css":
-		w.Header().Set("Content-Type", "text/css")
-		w.Header().Set("Cache-Control", "public, max-age=120")
-		fmt.Fprintf(w, "%s", customizationCSS)
-		return
 	}
 	http.Error(w, "error not found", http.StatusNotFound)
 }
