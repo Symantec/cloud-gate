6e69c554c00092814c99f29d4981dd0f2528e74c
Camilo Viecco <cviecco@gmail.com>
2018-11-11 22:08:08 -0800

    decrypt tests

--- broker/aws/impl_test.go
+++ broker/aws/impl_test.go
@@ -21,6 +21,17 @@ aws_secret_access_key = asdasdasdasdasdsad
 region = us-east-1
 `
 
+//same as above but encrypted with passphrase "password"
+const encryptedValidCredentials = `-----BEGIN PGP MESSAGE-----
+Comment: GPGTools - http://gpgtools.org
+
+jA0EBwMCPUCLUmxQYZvk0p0BFvgNP64N/PJY88/iC4599KKOIVvf44ceHsUqrg1q
+vS2FjMr4itQUd0e1j9mGFNNUMsHZDQ2mlB+yl9ZcfI3LfGiav/Uln7+iLlgSBNwH
+6YUWOLIg432i6KL5sD1jxasL+3ubzZoxia+g2Q240L82HcAWCnaCVv/z+2FnR7t4
+Gx3fQbU0jBkntZw2bHeUZnryMu6TC9hmyLl0q/Rz
+=Dp5J
+-----END PGP MESSAGE-----`
+
 func setupCachedBroker() *Broker {
 	slogger := stdlog.New(os.Stderr, "", stdlog.LstdFlags)
 	logger := debuglogger.New(slogger)
@@ -46,12 +57,12 @@ func setupCachedBroker() *Broker {
 }
 
 func TestLoadCredentialsFrombytesSuccess(t *testing.T) {
-	broker := setupCachedBroker()
-	c1, err := broker.GetIsUnsealedChannel()
+	b := setupCachedBroker()
+	c1, err := b.GetIsUnsealedChannel()
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = broker.loadCredentialsFrombytes([]byte(validTestPlaintextCredentials))
+	err = b.loadCredentialsFrombytes([]byte(validTestPlaintextCredentials))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -64,5 +75,27 @@ func TestLoadCredentialsFrombytesSuccess(t *testing.T) {
 	case <-time.After(500 * time.Millisecond): //500ms should be enough
 		t.Fatal("too slow")
 	}
+}
 
+func TestUnsealingSucess(t *testing.T) {
+	b := setupCachedBroker()
+
+	c1, err := b.GetIsUnsealedChannel()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b.rawCredentialsFile = []byte(encryptedValidCredentials)
+	_, err = b.ProcessNewUnsealingSecret("password")
+	if err != nil {
+		t.Fatal(err)
+	}
+	select {
+	case unsealErr := <-c1:
+		if unsealErr != nil {
+			t.Fatal(unsealErr)
+		}
+	case <-time.After(500 * time.Millisecond): //500ms should be enough
+		t.Fatal("too slow")
+	}
 }
